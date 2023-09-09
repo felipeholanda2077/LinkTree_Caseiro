fe0a4f02d036a27fb90aec0f06795aeeec89a905
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-09-09 19:19:51 -0300

    Tamanho da Imagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
         <Image
           src="/images/Eu.jpeg"
           alt="Felipe Holanda"
-          width={250}
-          height={250}
+          width={200}
+          height={200}
           border-radius={50}
           className="border-2 border-radius-50 rounded-full mb-4 imag"
         />
