aecd4378891ee5ff4a7e83cf229394021e0b531b
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2024-01-13 14:05:19 -0300

    Ajuste de tamanho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,10 +62,10 @@ export default function Home() {
         </p>
 
 
-        <ul className="flex flex-row grid grid-rows-3 grid-flow-col mr-2 ml-2">
+        <ul className="flex flex-row grid grid-rows-3">
           <li>
 
-            <div className="buttons">
+            <div className="buttons mr-2 ml-2">
               <a href="https://www.linkedin.com/in/felipe-holandadev/" target="_blank" rel="noopener noreferrer">
                 <button className="blob-btn w-full h-full">
 
